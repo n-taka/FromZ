d7277effa9f225f3134ebf9a95c15bbd79ee65cb
Kazutaka Nakashima <kazutaka.nakashima@n-taka.info>
2023-04-24 14:34:30 +0900

    [misc] add GPL-related statements for each file

--- src/writeGoZFile.cpp
+++ src/writeGoZFile.cpp
@@ -1,3 +1,17 @@
+//
+// Copyright (C) 2022-2023 Kazutaka Nakashima (kazutaka.nakashima@n-taka.info)
+// 
+// GPLv3
+//
+// This file is part of FromZ.
+// 
+// FromZ is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+// 
+// FromZ is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License along with FromZ. If not, see <https://www.gnu.org/licenses/>.
+//
+
 #ifndef writeGoZFile_CPP
 #define writeGoZFile_CPP
 
